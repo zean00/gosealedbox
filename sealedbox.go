035a990be9e791f20ac64ffac8f9602a320aae00
Sahal Zain <zean00zone@gmail.com>
2017-02-23 22:24:41 +0700

    add key conversion

--- sealedbox.go
+++ sealedbox.go
@@ -2,8 +2,11 @@ package gosealedbox
 
 import (
 	"crypto/rand"
+	"errors"
 
+	ex "github.com/agl/ed25519/extra25519"
 	"github.com/dchest/blake2b"
+	"golang.org/x/crypto/curve25519"
 	"golang.org/x/crypto/ed25519"
 	"golang.org/x/crypto/nacl/box"
 )
@@ -65,4 +68,39 @@ func SignMessageOpen(publicKey ed25519.PublicKey, sigmsg []byte) ([]byte, bool) 
 		m = nil
 	}
 	return m, r
+}
+
+//SignKeyToBox convert pair of signature key to pair of box key (secret, public, error)
+func SignKeyToBox(privateKey, publicKey []byte) (*[32]byte, *[32]byte, error) {
+	if len(privateKey) != ed25519.PrivateKeySize {
+		return nil, nil, errors.New("Invalid private key length")
+	}
+
+	if len(publicKey) != ed25519.PublicKeySize {
+		return nil, nil, errors.New("Invalid public key length")
+	}
+
+	var sk, pk, spk *[32]byte
+	var ssk [64]byte
+	copy(ssk[:], privateKey[0:64])
+	copy(spk[:], publicKey[0:32])
+	ex.PrivateKeyToCurve25519(sk, &ssk)
+	ex.PublicKeyToCurve25519(pk, spk)
+	return sk, pk, nil
+}
+
+//PrivateSignToBox convert signature private key to box key pair (secret, public, error)
+func PrivateSignToBox(privateKey []byte) (*[32]byte, *[32]byte, error) {
+	if len(privateKey) != ed25519.PrivateKeySize {
+		return nil, nil, errors.New("Invalid private key length")
+	}
+	pk := ed25519.PrivateKey(privateKey).Public()
+	return SignKeyToBox(privateKey, pk.([]byte))
+}
+
+//ToPublic convert box secret key to public key
+func ToPublic(privateKey *[32]byte) *[32]byte {
+	var pk *[32]byte
+	curve25519.ScalarBaseMult(pk, privateKey)
+	return pk
 }
